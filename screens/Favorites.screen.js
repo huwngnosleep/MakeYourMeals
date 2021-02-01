1fb8839f1947230dd608a046dc9757ec0568bcf0
huwngnosleep <thanhhung8210@gmail.com>
2021-02-01 12:13:15 +0700

    Created a new Expo app

---
+++ screens/Favorites.screen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, View, } from 'react-native'
+
+const Favorites = (props) => {
+    return(
+        <View style={}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Favorites
---
+++ screens/Filters.screen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, View, } from 'react-native'
+
+const Filters = (props) => {
+    return(
+        <View style={}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Filters
